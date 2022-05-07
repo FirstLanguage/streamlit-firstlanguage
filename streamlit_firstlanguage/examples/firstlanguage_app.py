7a6dea56e27bd756fbea94847a24e5758bbcd424
Subalalitha <79745769+subalalithafl@users.noreply.github.com>
2022-05-07 11:50:52 +0530

    test

--- streamlit_firstlanguage/examples/firstlanguage_app.py
+++ streamlit_firstlanguage/examples/firstlanguage_app.py
@@ -67,4 +67,5 @@ if nlpTask == "Summarization":
 if nlpTask == "Translation":
     fl.firstlanguage_translate("fl_translate",st.secrets["api_key"], title="Translation",resTitle="Translated Text")
     
-uploaded_file = st.file_uploader("Choose a file")    
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
